6e65f3b2f4b3de22f167fac9ef2bd25dcb50e4f3
Morgan Keith <morgan.a.keith92@gmail.com>
2024-08-21 12:01:22 -0500

    8/21/2024

--- Home.py
+++ Home.py
@@ -15,34 +15,6 @@ with open('style.css', 'r') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-#background image
-import base64
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('images/transparent background.png')
-
-
-
-
 # logo image
 st.image('images/Red graphic circle PoizdP.png',  width=500)    
 
